5be60391a2cf13d4ba49457b3465d6181937ff65
nikhidas <nikhildasnomula@ymail.com>
2018-05-19 03:53:55 -0400

    Creating the blockchain lottery app from Udemy

---
+++ blockchain-lottery/deploy.js
@@ -0,0 +1,23 @@
+const HDWalletProvider = require('truffle-hdwallet-provider');
+const Web3 = require('web3');
+const { interface, bytecode } = require('./compile');
+
+const provider = new HDWalletProvider(
+'tragic rigid snap drive outer infant act tired trip coffee voice unfair',
+  //'https://rinkeby.infura.io/e6w9tt2BMECtyZ9k6QTU'
+  'https://rinkeby.infura.io/uSt7BzH9NUQK0BuKZd5z'
+);
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+  const accounts = await web3.eth.getAccounts();
+  console.log('Attempting to deploy from account',accounts[0]);
+  const result = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({ data : '0x' + bytecode})
+    .send({ gas : '1000000', from : accounts[0] });
+
+  console.log(interface);
+
+  console.log('Contract deployed to', result.options.address);
+};
+deploy();
