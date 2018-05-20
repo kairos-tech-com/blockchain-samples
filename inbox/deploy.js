a49229d7abe7bc9e1e73bc6a07f9afbd1595a93b
nikhidas <nikhildasnomula@ymail.com>
2018-05-20 02:58:41 -0400

    Added some extra console logs to see the balance of the account

--- inbox/deploy.js
+++ inbox/deploy.js
@@ -6,23 +6,25 @@ const provider = new HDWalletProvider(
   'exchange account mixed tobacco just cradle provide leader slight wave retreat ivory',
   'https://rinkeby.infura.io/uSt7BzH9NUQK0BuKZd5z'
 );
+
 const web3 = new Web3(provider);
 
-const deploy = async () => {
+const deploy = async() => {
   const accounts = await web3.eth.getAccounts();
   const balance = await web3.eth.getBalance(accounts[0]);
 
-  console.log('Attempting to deploy from account',accounts[0]);
+  console.log('Attempting to deploy from account', accounts[0]);
   console.log('Account balance ', balance);
 
   const result = await new web3.eth.Contract(JSON.parse(interface))
       .deploy({
-        data : '0x' + bytecode})
+        data : '0x' + bytecode,
+        arguments: ['Hi there!']})
       .send({ gas : '1000000', gasPrice: web3.utils.toWei('2', 'gwei'), from : accounts[0] });
 
   const address = await result.options.address;
   console.log('Contract deployed to ', result.options.address);
 
-
 };
+
 deploy();
